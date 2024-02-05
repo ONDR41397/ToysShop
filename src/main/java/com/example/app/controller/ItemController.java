558fcc4b7d44fb3b2f328bd02838115473107071
taro <taro@abc.com>
2024-02-05 13:25:07 +0900

    ページ分割機能追加

--- src/main/java/com/example/app/controller/ItemController.java
+++ src/main/java/com/example/app/controller/ItemController.java
@@ -4,18 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.app.service.ItemService;
 
 @Controller
 public class ItemController {
+	
+	// 1ページ当たりの表示人数
+	private static final int NUM_PER_PAGE = 5;
 
 	@Autowired
 	ItemService service;
 
-	@GetMapping("/itemList")
-	public String itemList(Model model) throws Exception {
-		model.addAttribute("itemList", service.getItemList());
+	@GetMapping("/admin/itemList")
+	public String itemList(@RequestParam(name = "page", defaultValue = "1") Integer page, 
+			Model model) throws Exception {
+		model.addAttribute("items", service.getItemListByPage(page, NUM_PER_PAGE));
+		model.addAttribute("page", page);
+		model.addAttribute("totalPages", service.getTotalPages(NUM_PER_PAGE));
+		//model.addAttribute("itemList", service.getItemList());
 		return "itemList";
 	}
 }
