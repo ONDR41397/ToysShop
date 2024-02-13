1846919b6a7a940e60c4ff13b70a4d27e6df4ded
taro <taro@abc.com>
2024-02-13 11:24:18 +0900

    SQL追加

--- toysshop.sql
+++ toysshop.sql
@@ -1,41 +1,45 @@
 # DB作成
-CREATE SCHEMA `toysshopdb` ;
-use `toysshopdb` ;
+CREATE DATABASE toysshopdb;
+
+USE toysshopdb;
 
 # adminsテーブル作成
-CREATE TABLE `toysshopdb`.`admins` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20) NOT NULL,
-  `userId` VARCHAR(20) NOT NULL,
-  `pass` VARCHAR(60) NOT NULL,
-  `type` INT NOT NULL,
+CREATE TABLE `admins` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `userId` varchar(20) NOT NULL,
+  `pass` varchar(60) NOT NULL,
+  `type` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `userId_UNIQUE` (`userId` ASC) VISIBLE);
-  
-# adminsテーブルデータ作成
-INSERT INTO `toysshopdb`.`admins` (`id`, `name`, `userId`, `pass`, `type`) VALUES ('1', '山田 太郎', 'taro@abc.com', '$2a$08$4EqTe.iSHTtefvuygHjUS..j2NV0dWKUNr7IIHxVbpux4A/GczWtC', '1');
-INSERT INTO `toysshopdb`.`admins` (`id`, `name`, `userId`, `pass`, `type`) VALUES ('2', '伊藤 次郎', 'jiro@abc.com', '$2a$08$RLd5TyHIlmqvy3YvwxE1K.zZYkBJfd20ail94AHbM55CWNAPahnFa', '2');
+  UNIQUE KEY `userId_UNIQUE` (`userId`)
+);
 
-# itemsテーブルデータ作成
-CREATE TABLE `toysshopdb`.`items` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(10) NOT NULL,
-  `price` VARCHAR(10) NOT NULL,
-  `descript` VARCHAR(300) NOT NULL,
-  `pict` VARCHAR(300) NULL,
-  `status` CHAR(3) NOT NULL DEFAULT 'ACT',
-  `created` DATETIME NOT NULL,
-  PRIMARY KEY (`id`));
+# adminsテーブル、データ追加
+INSERT INTO `admins` 
+VALUES (1,'山田 太郎','taro@abc.com','$2a$08$4EqTe.iSHTtefvuygHjUS..j2NV0dWKUNr7IIHxVbpux4A/GczWtC',1),
+(2,'伊藤 次郎','jiro@abc.com','$2a$08$RLd5TyHIlmqvy3YvwxE1K.zZYkBJfd20ail94AHbM55CWNAPahnFa',2),
+(3,'田中 三郎','saburo@abc.com','$2a$08$gx2Vd3W/1Kf1NCU2Su174OHItSJ0wHWacjUGrG3W2sXmHPCqMs1gC',2);
 
-# itemsテーブルデータ作成
-INSERT INTO `toysshopdb`.`items` (`id`, `name`, `price`, `descript`, `status`, `created`) VALUES ('1', '商品A', '380', 'これは商品Aです', 'ACT', '2024-02-02 00:00:00');
-INSERT INTO `toysshopdb`.`items` (`id`, `name`, `price`, `descript`, `status`, `created`) VALUES ('2', '商品B', '2,980', 'これは商品Bです', 'ACT', '2024-02-02 00:00:00');
+# itemsテーブル作成
+CREATE TABLE `items` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `price` varchar(10) NOT NULL,
+  `descript` varchar(300) NOT NULL,
+  `pict` varchar(300) DEFAULT NULL,
+  `status` char(3) NOT NULL DEFAULT 'ACT',
+  `created` datetime NOT NULL,
+  `buyer` varchar(20) NOT NULL DEFAULT 'none',
+  `purchase` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ;
 
-# historysテーブル作成
-CREATE TABLE `toysshopdb`.`historys` (
-  `no` INT NOT NULL AUTO_INCREMENT FIRST,
-  `userId` INT NOT NULL,
-  `itemId` INT NOT NULL,
-  `cart` INT NOT NULL DEFAULT 0,
-  `purchaseDate` DATETIME NULL,
-  PRIMARY KEY (`no`));
+# itemsテーブル、データ追加
+INSERT INTO `items` 
+VALUES (1,'商品A','389','これは商品Aです','tomica.jpg','ACT','2024-02-06 16:31:59','none',0),
+(2,'商品B','5690','これは商品Bです','lego.jpg','ACT','2024-02-06 17:43:57','none',0),
+(3,'商品C','10000','これは商品Cです','ラジコン.jpg','ACT','2024-02-06 17:44:53','none',0),
+(4,'商品D','1000','これは商品Dです',NULL,'ACT','2024-02-13 00:00:00','none',0),
+(5,'商品E','3890','これは商品Eです',NULL,'ACT','2024-02-13 00:00:00','none',0),
+(6,'商品F','380','これは商品Fです',NULL,'ACT','2024-02-13 00:00:00','none',0),
+(7,'商品G','150','これは商品Gです',NULL,'ACT','2024-02-13 00:00:00','none',0);
